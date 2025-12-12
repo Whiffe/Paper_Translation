9987a85c6043daddd483f9b273b1919337cbc75f
Fan Yang <1349896465@qq.com>
2025-12-12 18:54:27 +0800

    up

---
+++ Safe/通过在激活空间中不鼓励其表示来避免越狱 — LessWrong --- Avoiding jailbreaks by discouraging their representation in activation space — LessWrong_files/43c708296ffdaa12.js
@@ -0,0 +1,5 @@
+;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="787e2266-a58b-9736-921c-9bc01c4dcec9")}catch(e){}}();
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,397675,o=>{o.n(o.i(110601))}]);
+
+//# debugId=787e2266-a58b-9736-921c-9bc01c4dcec9
+//# sourceMappingURL=dc2ccee97b8a8b91.js.map
